2eebd944577e0bd736dcc6d5f18031292ddce266
Quang dat <Quangdat570@gmail.com>
2022-11-25 17:34:54 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -9,17 +9,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 index: resolve(__dirname, "src/index.html"),
-                about: resolve(__dirname, "src/about.html"),
-                shop: resolve(__dirname, "src/shop.html"),
-                signin: resolve(__dirname, "src/signin.html"),
-                product: resolve(__dirname, "src/product.html"),
-                cart: resolve(__dirname, "src/cart.html"),
-                blog: resolve(__dirname, "src/blog.html"),
-                contact: resolve(__dirname, "src/contact.html"),
-                service: resolve(__dirname, "src/service.html"),
-                signup: resolve(__dirname, "src/signup.html"),
-                sign: resolve(__dirname, "src/sign.html"),
-                checkout: resolve(__dirname, "src/checkout.html"),
+                
             },
         },
     },
